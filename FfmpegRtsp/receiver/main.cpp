58ea13b9b1cb1fbeedaeda066d5a110cdc639b47
chenFan <956960783@qq.com>
2025-03-26 17:18:10 +0800

    优化ffmpeg参数

--- FfmpegRtsp/receiver/main.cpp
+++ FfmpegRtsp/receiver/main.cpp
@@ -4,8 +4,8 @@ int main(int, char **)
 {
     std::cout << "Hello, from receiver!\n";
 
-    // std::string rtsp_url = "rtsp://192.168.51.166:5554/user=admin&password=&channel=1&stream=0.sdp?";
-    std::string rtsp_url = "rtsp://192.168.147.128:8554/test";
+    std::string rtsp_url = "rtsp://192.168.51.166:5554/user=admin&password=&channel=1&stream=0.sdp?";
+    // std::string rtsp_url = "rtsp://192.168.147.128:8554/test";
     RTSPStream stream(rtsp_url);
     stream.start();
 
