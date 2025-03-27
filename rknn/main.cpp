88d1c135706efa307988ff6697be71143fa2068b
chen-9824 <956960783@qq.com>
2025-03-27 10:56:34 +0800

    测试

--- rknn/main.cpp
+++ rknn/main.cpp
@@ -2,21 +2,21 @@
 #include "rknn.h"
 #include "opencv2/opencv.hpp"
 
-std::string image_name = "../rknn_yolov5/model/test2.jpg";
+std::string image_name = "./test.jpg";
 
 int main()
 {
     std::cout << "Hello, AArch64!" << std::endl;
-    Rknn rknn("../rknn_yolov5/model/rk180x/yolov5s_relu_rk180x_out_opt.rknn", "../rknn_yolov5/model/rk180x/coco_80_labels_list.txt", 80, 80 + 5);
+    Rknn rknn("../rknn/rknn_yolov5/model/rk3566/yolov5_3566.rknn", "../rknn/rknn_yolov5/model/coco_80_labels_list.txt", 80, 80 + 5);
 
     cv::Mat rgbFrame;
 
     if (rknn.init() == 0)
     {
-        cv::Mat orig_img = cv::imread(image_name, 1);
-        if (!orig_img.data)
+        cv::Mat orig_img = cv::imread(image_name, cv::IMREAD_COLOR);
+        if (orig_img.empty())
         {
-            printf("cv::imread %s fail!\n", image_name);
+            printf("cv::imread %s fail!\n", image_name.c_str());
             return -1;
         }
         cv::cvtColor(orig_img, rgbFrame, cv::COLOR_BGR2RGB);
